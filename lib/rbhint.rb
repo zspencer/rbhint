04fa0fadcc858278753f122b7ddd9903b1ab1d30
Zee <50284+zspencer@users.noreply.github.com>
2020-06-08 19:56:40 -0700

    Provide Documentation for the RbHint namespace

--- lib/rbhint.rb
+++ lib/rbhint.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require 'rubocop'
-
+# A Kindler, Gentler Ruby Linter.
+#
+# RbHint encourages community norms through notices and warnings
+# as part of your development process. It may also enforce firm
+# boundaries when the boundaries are appropriate for your team.
+#
 module RbHint
 end
