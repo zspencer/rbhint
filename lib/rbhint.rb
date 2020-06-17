a6577edca6a452be856ef1c7ab645805be3f9762
Zee <50284+zspencer@users.noreply.github.com>
2020-06-17 10:26:23 -0700

    [#13] Document purpose of RbHint (#34)
    * [#15] Resolve outstanding line length flag that was breaking CI

--- lib/rbhint.rb
+++ lib/rbhint.rb
@@ -2,5 +2,13 @@
 
 require 'rubocop'
 
+# A Kindler, Gentler Ruby Linter and Formatter.
+#
+# RbHint encourages a shared code format by giving hints and
+# flagging unusually formatted code in a project.
+#
+# It may also be used to enforce firm boundaries for code format
+# when those boundaries are appropriate for the project.
+#
 module RbHint
 end
